9477c9a1b90b7b35b650a99f3f465d4b69c5180f
Guilherme Segatto <segattooguilherme@gmail.com>
2024-03-15 11:03:24 -0300

    continuação aula05

--- Back-end/aula05/index.test.js
+++ Back-end/aula05/index.test.js
@@ -9,3 +9,52 @@ test("Deve retornar status 200 no GET", async function(){
     expect(response.status).toBe(200);
     expect(response.headers['content-type']).toMatch(/json/);
 })
+
+test("Deve retornar status 200 e um Json no GET id", async function(){
+    const response = await request.get("/produtos/1");
+    expect(response.status).toBe(200);
+    expect(response.headers['content-type']).toMatch(/json/);
+});
+
+test("Deve retornar status 404 e um Json no GET id", async function(){
+    const response = await request.get("/produtos/100");
+    expect(response.status).toBe(404);
+    expect(response.headers['content-type']).toMatch(/json/);
+});
+
+test("Deve retornar status 201 e um JSON no post", async function(){
+    const response = await request.post("/produtos").send({nome: "Banana-nanica", preco: 15.00});
+    expect(response.status).toBe(201);
+    expect(response.headers['content-type']).toMatch(/json/);
+});
+
+test("Deve retornar status 422 e um JSON no post", async function(){
+    const response = (await request.post("/produtos"));
+    expect(response.status).toBe(422);
+    expect(response.headers['content-type']).toMatch(/json/);
+});
+
+test("Deve retornar status 200 e um JSON no put", async function(){
+    const response = await request.put("/produtos/1").send({nome: "Banana-prata", preco: 20.00});
+    expect(response.status).toBe(200);
+    expect(response.headers['content-type']).toMatch(/json/);
+});
+
+test("Deve retornar status 404 e um JSON no put", async function(){
+    const response = await request.put("/produtos/100");
+    expect(response.status).toBe(404);
+    expect(response.headers['content-type']).toMatch(/json/);
+});
+
+test("Deve retornar status 204 e sem Body no Delete", async function(){
+    const response = await request.delete("/produtos/1");
+    expect(response.status).toBe(204);
+    expect(response.body).toEqual({});
+});
+
+test("Deve retornar status 404 e um JSON no Delete", async function(){
+    const response = await request.delete("/produtos/100");
+    expect(response.status).toBe(404);
+    expect(response.headers['content-type']).toMatch(/json/);
+});
+
--- Back-end/aula05/router.js
+++ Back-end/aula05/router.js
@@ -3,7 +3,41 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/produtos", function(req, res){
- res.json([])
+ res.json([]) // 200 e json.
+});
+
+router.get("/produtos/:produtoId", function(req, res){
+    if (req.params.produtoId == 1){
+        res.json({}) // 200 e json.
+    } else {
+        res.status(404).json({msg: "Produto não encontrado"})
+    }
+ 
+});
+
+router.post("/produtos", function(req, res){
+  if (req.body && req.body.nome && req.body.preco){
+    res.status(201).json({})
+  } else {
+    res.status(422).json({msg: "Nome e preco do  produto obrigatorio!"})
+  }
+});
+
+router.put("/produtos/:produtoId", function(req, res){
+   if ( req.params.produtoId == 1) {
+    res.json({});
+   } else {
+    res.status(404).json({msg: "Produtos não encontrado"})
+   }
+});
+
+router.delete("/produtos/:produtoId", function(req, res){
+    if ( req.params.produtoId == 1 ){
+        res.status(204).end();
+    } else {
+        res.status(404).json({msg: "Produto não encontrado."})
+    }
+    
 });
 
 module.exports = router;
