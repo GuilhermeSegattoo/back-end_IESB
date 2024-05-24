4e83073b7ae4207aebbfd2e96066e1116db4d22d
Guilherme Segatto <segattooguilherme@gmail.com>
2024-05-24 10:55:29 -0300

    aula11

--- Aulas/aula11/routes/users.js
+++ Aulas/aula11/routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
+const controller = require("../controllers/users");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post("/", controller.criar );
+router.post("/login", controller.entrar);
 
 module.exports = router;
